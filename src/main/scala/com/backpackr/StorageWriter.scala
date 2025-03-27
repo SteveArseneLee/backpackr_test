9fbdd8c0b3cc5b6378180b1c720ea143af4f77d4
ChangRyeol Lee <lclgood@naver.com>
2025-03-27 14:45:09 +0900

    Step 3. 재처리 후 parquet, snappy 처리

--- src/main/scala/com/backpackr/StorageWriter.scala
+++ src/main/scala/com/backpackr/StorageWriter.scala
@@ -1,5 +1,21 @@
 package com.backpackr
 
-object StorageWriter {
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
+object StorageWriter {
+  /**
+   * parquet + snappy 포맷으로 데이터를 저장하는 함수
+   * @param df 저장할 DataFrame
+   * @param outputPath 저장 경로
+   */
+  def writeAsPartitionedParquet(df: DataFrame, outputPath: String)(implicit spark: SparkSession): Unit = {
+    df.write
+      .format("parquet")
+      .option("compression", "snappy")
+      .mode(SaveMode.Overwrite)
+      .partitionBy("partition_date")
+      .save(outputPath)
+
+    println(s"저장 완료: $outputPath")
+  }
 }

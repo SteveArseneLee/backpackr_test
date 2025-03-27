3dda0d139c3b4d37851af30ec000305d03f1e4ac
ChangRyeol Lee <lclgood@naver.com>
2025-03-27 14:41:43 +0900

    Step 1. KST 기준 daily partition 처리

--- src/main/scala/com/backpackr/PartitionWriter.scala
+++ src/main/scala/com/backpackr/PartitionWriter.scala
@@ -3,19 +3,16 @@ package com.backpackr
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
-/**
- * KST 기준 partition_date column 추가
- * @param df : 원본 DataFrame (event_time : timestamp 포함)
- * @return partition_date : column 추가된 DataFrame
- */
-
 object PartitionWriter {
-   def addKSTPartitionColumn(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
-     import spark.implicits._
+  /**
+   * event_time(UTC) → event_time_kst 컬럼 추가 후,
+   * yyyyMMdd 기준으로 partition_date 컬럼 추가
+   */
+  def addKSTPartitionColumns(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    import spark.implicits._
 
-     df.withColumn("event_time_ts", to_timestamp($"event_time"))
-       .withColumn("event_time_kst", expr("event_time_ts + INTERVAL 9 HOURS"))
-       .withColumn("partition_date", date_format($"event_time_kst", "yyyyMMdd"))
-       .drop("event_time_ts")
-   }
+    df
+      .withColumn("event_time_kst", expr("event_time + INTERVAL 9 HOURS")) // 한국시간 변환
+      .withColumn("partition_date", date_format($"event_time_kst", "yyyyMMdd")) // 파티션용 날짜 추출
+  }
 }
